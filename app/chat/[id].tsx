87c261d72dbee1eb924b013b64ad33c0081dc5b4
desireland-pixel <desireland13@gmail.com>
2026-03-05 13:03:50 +0100

    Update [id].tsx

--- app/chat/[id].tsx
+++ app/chat/[id].tsx
@@ -492,7 +492,8 @@ export default function ChatScreen() {
           renderItem={renderMessage}
           keyExtractor={(item) => item.id}
           style={styles.messagesContainer}
-          contentContainerStyle={styles.messagesContent}
+          contentContainerStyle={[styles.messagesContent, { paddingBottom: 120 }]}
+          keyboardShouldPersistTaps="handled"
           onContentSizeChange={() => flatListRef.current?.scrollToEnd({ animated: false })}
         />
 
