e07d921b34f3b8427afcbb5fa7e546524a6c9695
desireland-pixel <desireland13@gmail.com>
2026-03-09 22:14:28 +0100

    Update [id].tsx

--- app/community/[id].tsx
+++ app/community/[id].tsx
@@ -385,7 +385,7 @@ export default function CommunityDetailsScreen() {
       <KeyboardAvoidingView 
         behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
         style={styles.keyboardView}
-        keyboardVerticalOffset={Platform.OS === 'ios' ? 90 : 75}
+        keyboardVerticalOffset={Platform.OS === 'ios' ? 95 : 75}
       >
         <ScrollView 
           style={styles.content}
