361323ea1a2c84e8cb95f689d7a0baeb644cd542
natively.dev <api@natively.dev>
2026-05-22 20:54:28 +0000

    website: agent run failed -- unable to open database file

--- website/.next/static/chunks/webpack.js
+++ website/.next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "3162c5a609a2eee8"; }
+/******/ 		__webpack_require__.h = function() { return "1d61f6989129af5a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
